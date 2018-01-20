ffbfc7f4bbe055b1e4451e3895fad5ee507db00d
Jeffrey <jwan622@gmail.com>
2018-01-20 00:46:58 -0500

    finish connection class

--- lib/jobcoin_client.rb
+++ lib/jobcoin_client.rb
@@ -2,5 +2,4 @@ require "jobcoin_client/version"
 require 'jobcoin_client/connection'
 
 module JobcoinClient
-  # Your code goes here...
 end
--- lib/jobcoin_client/connection.rb
+++ lib/jobcoin_client/connection.rb
@@ -4,10 +4,11 @@ class Connection
   attr_reader :hostname
 
   def initialize(hostname)
+    # remember that net/http does not want the http part
     @hostname = hostname
   end
 
   def get(uri)
-    Net::HTTP.get(hostname, '/protozoan/api/transactions')
+    Net::HTTP.get(hostname, uri)
   end
 end
