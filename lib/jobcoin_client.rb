8c4875f8e5830a0f2e91f5b913e6ee4675b9d2be
Jeffrey <jwan622@gmail.com>
2018-01-24 16:28:31 -0500

    refactor gem

--- lib/jobcoin_client.rb
+++ lib/jobcoin_client.rb
@@ -1,4 +1,5 @@
 require "jobcoin_client/version"
+require 'json'
 Dir["./lib/jobcoin_client/requests/*.rb"].each {|file| require file }
 
 # one way to use pry only in development mode. In prod, this will raise a LoadError
@@ -9,4 +10,34 @@ rescue LoadError => err
 end
 
 module JobcoinClient
+  class Jobcoin
+    attr_reader :connection
+
+    def initialize
+      @connection = Requests::Connection.new('http://jobcoin.gemini.com')
+    end
+
+    def transaction_history
+      res = connection.get('/protozoan/api/transactions')
+
+      JSON.parse(res)
+    end
+
+    def address_transactions(address)
+      res = connection.get("/protozoan/api/addresses/#{address}")
+
+      JSON.parse(res)
+    end
+
+    def add_transaction(from, to, amount)
+      payload = {
+        'fromAddress' => from,
+        'toAddress' => to,
+        'amount' => amount
+      }
+
+      res = connection.post("/protozoan/api/transactions", payload)
+      JSON.parse(res.body)
+    end
+  end
 end
